bb8621107ff49738156a3352f56ab5467623aa98
Matthias Endler <matthias-endler@gmx.net>
2021-02-13 17:35:49 +0100

    Add support for author/committer and date range

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -13,16 +13,46 @@ use anyhow::{Context, Result};
 use client::Parser;
 use duct::cmd;
 use std::fs;
+use structopt::StructOpt;
 
-fn get_commits() -> Result<String> {
-    Ok(cmd!("git", "log", "--date=short", "--pretty=format:%ad")
+mod options;
+use options::Opt;
+
+fn get_commits(
+    before: Option<String>,
+    after: Option<String>,
+    authors: Vec<String>,
+    commiters: Vec<String>,
+) -> Result<String> {
+    let mut args = vec!["log", "--date=short", "--pretty=format:%ad"];
+    if let Some(before) = &before {
+        args.push("--before");
+        args.push(before);
+    }
+    if let Some(after) = &after {
+        args.push("--after");
+        args.push(after);
+    }
+    for author in &authors {
+        args.push("--author");
+        args.push(author);
+    }
+    for committer in &commiters {
+        args.push("--committer");
+        args.push(committer);
+    }
+    let cmd = cmd("git", &args)
         .pipe(cmd!("sort"))
         .pipe(cmd!("uniq", "-c"))
-        .read()?)
+        .read()?;
+    Ok(cmd)
 }
 
 fn main() -> Result<()> {
-    let input = get_commits().context("Cannot read project history")?;
+    let opt = Opt::from_args();
+
+    let input = get_commits(opt.before, opt.after, opt.author, opt.committer)
+        .context("Cannot read project history")?;
     let mut parser = Parser::new(input);
     let timeline = parser.parse()?;
     let output = serde_json::to_string_pretty(&timeline)?;
---
+++ src/bin/client/options.rs
@@ -0,0 +1,25 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "client")]
+pub struct Opt {
+    /// Limit the commits output to ones with author header lines
+    /// that match the specified pattern.
+    /// This is passed verbatim to git. See `git log --help` for more info.
+    #[structopt(short, long)]
+    pub author: Vec<String>,
+
+    /// Limit the commits output to ones with committer header lines
+    /// that match the specified pattern.
+    /// This is passed verbatim to git. See `git log --help` for more info.
+    #[structopt(short, long)]
+    pub committer: Vec<String>,
+
+    // Show commits older than a specific date.
+    #[structopt(alias = "until", long)]
+    pub before: Option<String>,
+
+    // Show commits more recent than a specific date.
+    #[structopt(alias = "since", long)]
+    pub after: Option<String>,
+}
