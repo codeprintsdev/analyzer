250b815d6e14d7263250a41c17b92e63c15d0f7b
Matthias Endler <matthias-endler@gmx.net>
2021-02-18 15:09:00 +0100

    Add subcommand for merge

--- src/bin/analyzer/options.rs
+++ src/bin/analyzer/options.rs
@@ -2,24 +2,27 @@ use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "codeprints-analyzer")]
-pub struct Opt {
-    /// Limit the commits output to ones with author header lines
-    /// that match the specified pattern.
-    /// This is passed verbatim to git. See `git log --help` for more info.
-    #[structopt(short, long)]
-    pub author: Vec<String>,
+pub enum Command {
+    Run {
+        /// Limit the commits output to ones with author header lines
+        /// that match the specified pattern.
+        /// This is passed verbatim to git. See `git log --help` for more info.
+        #[structopt(short, long)]
+        author: Vec<String>,
 
-    /// Limit the commits output to ones with committer header lines
-    /// that match the specified pattern.
-    /// This is passed verbatim to git. See `git log --help` for more info.
-    #[structopt(short, long)]
-    pub committer: Vec<String>,
+        /// Limit the commits output to ones with committer header lines
+        /// that match the specified pattern.
+        /// This is passed verbatim to git. See `git log --help` for more info.
+        #[structopt(short, long)]
+        committer: Vec<String>,
 
-    // Show commits older than a specific date.
-    #[structopt(alias = "until", long)]
-    pub before: Option<String>,
+        // Show commits older than a specific date.
+        #[structopt(alias = "until", long)]
+        before: Option<String>,
 
-    // Show commits more recent than a specific date.
-    #[structopt(alias = "since", long)]
-    pub after: Option<String>,
+        // Show commits more recent than a specific date.
+        #[structopt(alias = "since", long)]
+        after: Option<String>,
+    },
+    Merge {},
 }
