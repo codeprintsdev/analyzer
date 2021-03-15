e80175cb57f4cd9990ff6170ff7e560db56ccc06
Matthias Endler <matthias-endler@gmx.net>
2021-03-15 14:36:40 +0100

    formatting

--- tests/test_merger.rs
+++ tests/test_merger.rs
@@ -4,7 +4,6 @@ use std::fs;
 
 #[test]
 fn test_intesity() {
-
     let input = fs::read_to_string("fixtures/mre_raw_2020_from_api.json").unwrap();
     let timeline: Timeline = serde_json::from_str(&input).unwrap();
     let mut merger = Merger::new();
@@ -18,7 +17,15 @@ fn test_intesity() {
     }
 
     for contribution in new_timeline.contributions {
-        let orig_contribution =  timeline.contributions.iter().find(|c| c.date == contribution.date).unwrap();
+        let orig_contribution = timeline
+            .contributions
+            .iter()
+            .find(|c| c.date == contribution.date)
+            .unwrap();
+
+        println!("contribution: {:?}", contribution);
+        println!("orig: {:?}", orig_contribution);
+
         assert_eq!(contribution.count, orig_contribution.count);
         assert_eq!(contribution.color, orig_contribution.color);
         assert_eq!(contribution.intensity, orig_contribution.intensity);
